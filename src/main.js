6e77ebce8cca7c891f0a3847aae15cf9c84aea12
liukai <liukai@huayun.com>
2021-04-07 17:58:53 +0800

    ui按需引入

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
-import Vue from 'vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import App from './App.vue';
+import Vue from 'vue'
+import 'element-ui/lib/theme-chalk/index.css'
+import App from './App.vue'
+import { Button, Form, FormItem, Table, TableColumn, Dialog, InputNumber, Input, Radio, RadioGroup } from 'element-ui'
 
-Vue.use(ElementUI);
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(Table)
+Vue.use(Dialog)
+Vue.use(InputNumber)
+Vue.use(Input)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(TableColumn)
+Vue.use(FormItem)
 
 Vue.config.productionTip = false
 
